7b24e123138920b8c34f8e6c7228e28fda179a4c
pedrovisk <pedro.h.marcondes.rocha@gmail.com>
2025-06-10 18:43:38 -0300

    shared e afins

---
+++ RegistroGames.API/RegistroGames.Shared.Dados/Banco/RegistroGamesContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using RegistroDeGames.Modelos;
+using RegistroGames.Shared.Dados.Modelos;
+
+namespace RegistroDeGames.Banco;
+
+public class RegistroGamesContext : IdentityDbContext<PessoaComAcesso, PerfilDeAcesso, int>
+{
+    public DbSet<Desenvolvedora> Desenvolvedoras { get; set; }
+
+    public DbSet<Jogo> Jogos { get; set; }
+
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RegistroGamesV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
---
+++ RegistroGames.API/RegistroGames.Shared.Dados/Migrations/20250602193811_RelacionarDevJogo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroDeGames.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelacionarDevJogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DesenvolvedoraId",
+                table: "Jogos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogos_DesenvolvedoraId",
+                table: "Jogos",
+                column: "DesenvolvedoraId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jogos_Desenvolvedoras_DesenvolvedoraId",
+                table: "Jogos",
+                column: "DesenvolvedoraId",
+                principalTable: "Desenvolvedoras",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jogos_Desenvolvedoras_DesenvolvedoraId",
+                table: "Jogos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jogos_DesenvolvedoraId",
+                table: "Jogos");
+
+            migrationBuilder.DropColumn(
+                name: "DesenvolvedoraId",
+                table: "Jogos");
+        }
+    }
+}
