ddb88666f8459b0fe4d73486cd0b4566d3f7c3d0
pedrovisk <pedro.h.marcondes.rocha@gmail.com>
2025-06-12 16:33:27 -0300

    Versão de Console

---
+++ RegistroDeGames/RegistroDeGames/Menus/MenuRegistrarDesenvolvedoras.cs
@@ -0,0 +1,22 @@
+﻿using RegistroDeGames.Banco;
+using RegistroDeGames.Modelos;
+
+namespace RegistroDeGames.Menus;
+
+internal class MenuRegistrarDesenvolvedoras : Menu
+{
+    public override void Executar(DAL<Desenvolvedora> desenvolvedoraDAL)
+    {
+        base.Executar(desenvolvedoraDAL);
+        ExibirTituloDaOpcao("Registro de Desenvolvedoras");
+        Console.Write("Digite o nome da desenvolvedora que deseja registrar: ");
+        string nomeDaDesenvolvedora = Console.ReadLine()!;
+        Console.Write("Digite a bio da desenvolvedora que deseja registrar: ");
+        string bioDaDesenvolvedora = Console.ReadLine()!;
+        Desenvolvedora desenvolvedora = new Desenvolvedora(nomeDaDesenvolvedora, bioDaDesenvolvedora);
+        desenvolvedoraDAL.Adicionar(desenvolvedora);
+        Console.WriteLine($"A desenvolvedora {nomeDaDesenvolvedora} foi registrada com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+}
---
+++ RegistroDeGames/RegistroDeGames/Menus/MenuRegistrarJogos.cs
@@ -0,0 +1,35 @@
+﻿using RegistroDeGames.Banco;
+using RegistroDeGames.Modelos;
+
+namespace RegistroDeGames.Menus;
+
+internal class MenuRegistrarJogos : Menu
+{
+    public override void Executar(DAL<Desenvolvedora> desenvolvedoraDAL)
+    {
+        base.Executar(desenvolvedoraDAL);
+        ExibirTituloDaOpcao("Registro de jogos");
+        Console.Write("Digite a desenvolvedora cujo jogo deseja registrar: ");
+        string nomeDaDesenvolvedora = Console.ReadLine()!;
+        var desenvolvedoraRecuperada = desenvolvedoraDAL.RecuperarPor(a => a.Nome.Equals(nomeDaDesenvolvedora));
+        if (desenvolvedoraRecuperada is not null)
+        {
+            Console.Write("Agora digite o título do jogo: ");
+            string tituloDoJogo = Console.ReadLine()!;
+            Console.Write("Agora digite o ano de lançamento do jogo: ");
+            string lancamentoDoJogo = Console.ReadLine()!;
+            desenvolvedoraRecuperada.AdicionarJogo(new Jogo(tituloDoJogo) { AnoLancamento = Convert.ToInt32(lancamentoDoJogo)});
+            Console.WriteLine($"O jogo {tituloDoJogo} de {nomeDaDesenvolvedora} foi registrado com sucesso!");
+            desenvolvedoraDAL.Atualizar(desenvolvedoraRecuperada);
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"\nA desenvolvedora {nomeDaDesenvolvedora} não foi encontrada!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
